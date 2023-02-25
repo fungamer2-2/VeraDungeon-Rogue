bf042085f5c5150196973d92b83f198706494784
grandsonneo@gmail.com <grandsonneo@gmail.com>
2023-02-25 14:31:40 -0500

    Commit

--- roguelike.py
+++ roguelike.py
@@ -43,9 +43,6 @@ if __name__ == "__main__":
 			g.maybe_load_game()	
 		if not g.has_saved_game(): #Either it failed to load or the player decided to start a new game
 			g.generate_level()
-			item = SummonScroll
-			for _ in range(100):
-				g.player.inventory.append(item())
 		for w in dup_warnings:
 			g.print_msg(f"WARNING: {w}", "yellow")	
 		g.draw_board()
