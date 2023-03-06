7cfc9fb2fe5e3f3aaaa3d1d1a994fb6ff8235e9f
grandsonneo@gmail.com <grandsonneo@gmail.com>
2023-03-06 12:47:08 -0500

    Commit

--- roguelike.py
+++ roguelike.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
 			g.maybe_load_game()	
 		if not g.has_saved_game(): #Either it failed to load or the player decided to start a new game
 			g.generate_level()
-			g.player.inventory.append(ClairPotion())
 		for w in dup_warnings:
 			g.print_msg(f"WARNING: {w}", "yellow")	
 		g.draw_board()
